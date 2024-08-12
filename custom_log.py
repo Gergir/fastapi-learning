8c417d471b73c89e986a67737c5f9d10c11728c3
Dawid Gruszka <aloneviahome@gmail.com>
2024-08-12 18:52:48 +0200

    added dependencies

--- custom_log.py
+++ custom_log.py
@@ -1,4 +1,8 @@
-def log(tag="", message=""):
-    with open("log.txt", "w+") as log_file:
+from fastapi import Request
+
+def log(tag="API call", message="", request: Request = None):
+    with open("log.txt", "a+") as log_file:
         log_file.write(f"{tag}: {message}\n")
+        if request:
+            log_file.write(f"\t {request.url}\n")
 
--- routers/dependency.py
+++ routers/dependency.py
@@ -1,17 +1,28 @@
 from fastapi import APIRouter, Request, Depends
+from custom_log import log
+router = APIRouter(prefix="/dependencies", tags=["dependencies"],
+                   dependencies=[Depends(log)])
 
-router = APIRouter(prefix="/dependencies", tags=["dependencies"])
+
+def convert_params(request: Request, separator: str):
+    query = []
+    for key, value in request.query_params.items():
+        query.append(f"{key} {separator} {value}")
+        return query
 
 
-def convert_header(request: Request)
+def convert_header(request: Request, separator: str, query = Depends(convert_params)):
     out_headers = []
     for key, value in request.headers.items():
-        out_headers.append(f"{key}: {value}")
-        return out_headers
+        out_headers.append(f"{key} {separator} {value}")
+        return {
+            "headers": out_headers,
+            "query": query
+        }
 
 
 @router.get("/")
-def get_items(headers=Depends(convert_header)):
+def get_items(separator: str = "-:-", headers=Depends(convert_header)):
     return {
         "items": ["a", "b", "c"],
         "headers": headers
@@ -22,3 +33,17 @@ def get_items(headers=Depends(convert_header)):
 def create_item(headers=Depends(convert_header)):
     return headers
 
+
+class Account:
+    def __init__(self, name: str, email: str):
+        self.name = name
+        self.email = email
+
+
+@router.post("/user")
+def create_accoount(name: str, email: str, password: str, account: Account = Depends()):
+    return {
+        "name": account.name,
+        "email": account.email,
+
+    }
